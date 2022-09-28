d30192b6151dd963c1056ff5d8c9dd70c8d9840b
BMDCNTT56 <demamavsdungmama@gmail.com>
2022-09-28 23:03:55 +0700

    #2 - create lab schema

---
+++ src/lab/lab.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const labSchema = new Schema(
+    {
+        labId: { type: String, unique: true, required: true },
+        labName: { type: String, required: true},
+        userCreated: { type: String, required: true},
+        trainLogPath: { type: String, required: true, unique: true },
+        testLogPath: { type: String, required: true, unique: true },
+        trainedModelPath: { type: String, required: true, unique: true },
+        configPath: { type: String, required: true, unique: true},
+    },
+    {
+        timestamps: {
+            createdAt: "createdAt",
+            updatedAt: "updatedAt",
+        },
+    }
+);
+
+const Lab = mongoose.model("model", labSchema);
+module.exports = Lab;
