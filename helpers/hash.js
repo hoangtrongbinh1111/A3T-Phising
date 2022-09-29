a7e5e812d20fb44b8ad3a19eb364b7bb4593c1d8
BMDCNTT56 <demamavsdungmama@gmail.com>
2022-09-29 23:11:14 +0700

    rs

--- helpers/hash.js
+++
@@ -1,5 +0,0 @@
-var crypto = require('crypto');
-
-exports.hashMD5 = (string) => {
-    return crypto.createHash('md5').update(string).digest("hex");
-}
--- routes/lab.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cleanBody = require("../middlewares/cleanbody");
-const labController = require("../src/lab/lab.controller");
-
-router.post("/create", cleanBody, labController.createLab);
-
-module.exports = router;
