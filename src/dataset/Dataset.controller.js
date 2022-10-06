f89df2627db8275ae4e46a68d3e854def1c58ba5
bmd_cntt_k56 <demamavsdungmama@gmail.com>
2022-10-06 21:02:48 +0700

    #7 delete lab or dataset file when delete on db

--- src/dataset/Dataset.controller.js
+++ src/dataset/Dataset.controller.js
@@ -1,94 +1,108 @@
-const Joi = require("joi");  //validate
+const Joi = require("joi"); //validate
 require("dotenv").config();
-const { v4: uuid } = require("uuid");//gen id
+const { v4: uuid } = require("uuid"); //gen id
 const dataset = require("./Dataset.model");
-const { responseServerError, responseSuccess, responseInValid, responseSuccessWithData } = require("../../helpers/ResponseRequest"); //response server
-const path = require('path') //work with path
-const { getDir } = require('../../helpers/file');  // create dir
+const {
+responseServerError,
+responseSuccess,
+responseInValid,
+responseSuccessWithData,
+} = require("../../helpers/ResponseRequest"); //response server
+const path = require("path"); //work with path
+const { getDir,removeDir } = require("../../helpers/file"); // create dir
 const { DATA_FOLDER, DATA_SUBFOLDER } = require("../../helpers/constant");
 const { VirtualType } = require("mongoose");
 
-
 const updateDatasetSchema = Joi.object().keys({
-    datasetId: Joi.string().required(),
-    numTrain: Joi.number().optional(),
-    numVal: Joi.number().optional(),
-    numTest: Joi.number().optional(),
-    dataType: Joi.string().optional(),
-    desc: Joi.string().optional()
-})
+datasetId: Joi.string().required(),
+numTrain: Joi.number().optional(),
+numVal: Joi.number().optional(),
+numTest: Joi.number().optional(),
+dataType: Joi.string().optional(),
+desc: Joi.string().optional(),
+});
 const datasetCreateSchema = Joi.object().keys({
-    userUpload: Joi.string().required(),
-    dataName: Joi.string().required(),
-    savePath: Joi.string(),
-})
+userUpload: Joi.string().required(),
+dataName: Joi.string().required()
+});
 exports.updateDataset = async (req, res) => {
-    try {
-       
-            const result = updateDatasetSchema.validate(req.body)
-            if (result.error) {
-                return responseServerError({ res, err: result.error.message })
-            }
-            let data = await dataset.findOneAndUpdate(req.body.datasetId, req.body, {
-                new: true
-            })
-            return responseSuccessWithData({
-                res, data: data
-            })
-
-    } catch (err) {
-
-        return responseServerError({ res, err: err.message })
-    }
+try {
+const result = updateDatasetSchema.validate(req.body);
+if (result.error) {
+return responseServerError({ res, err: result.error.message });
 }
+delete result.value.datasetId;
+let data = await dataset.findOneAndUpdate(req.body.datasetId, result.value, {
+new: true,
+});
+return responseSuccessWithData({
+res,
+data: data,
+});
+} catch (err) {
+return responseServerError({ res, err: err.message });
+}
+};
 exports.createDataset = async (req, res) => {
-    try {
-        const result = datasetCreateSchema.validate(req.body);
-        if (result.error) {
-            return responseServerError({ res, err: result.error.message });
-        }
-        const { dataName, userUpload} = req.body;
-        const datasetId = uuid();
-        const savePath = `/${DATA_FOLDER}/${datasetId}`
-        //create folder 
-        const root = path.resolve('./');
-        const dir = getDir({ dir: root + `/${DATA_FOLDER}` });
-        const dataDir = getDir({ dir: root + `/${DATA_FOLDER}/${req.body.dataName}` })
-        Object.keys(DATA_SUBFOLDER).map((subfolder) => {
-            getDir({ dir: root + `/${DATA_FOLDER}/${req.body.dataName}/${DATA_SUBFOLDER[subfolder]}` })
-        })
-        const data = {
-            datasetId,
-            dataName,
-            userUpload,
-            savePath,
-        }
-        const newData = new dataset(data);
-        await newData.save();
-        return responseSuccessWithData({ res, data: newData });
-    } catch (error) {
-        return responseServerError({ res, err: error.message });
-    }
-
+try {
+const result = datasetCreateSchema.validate(req.body);
+if (result.error) {
+return responseServerError({ res, err: result.error.message });
 }
+const { dataName, userUpload } = req.body;
+const datasetId = uuid();
+const savePath = `/${DATA_FOLDER}/${datasetId}`;
+//create folder
+const root = path.resolve("./");
+// const dir = getDir({ dir: root + `/${DATA_FOLDER}` });
+const dataDir = getDir({
+dir: root + `/${DATA_FOLDER}/${datasetId}`,
+});
+Object.keys(DATA_SUBFOLDER).map((subfolder) => {
+getDir({
+dir:
+root +
+`/${DATA_FOLDER}/${datasetId}/${DATA_SUBFOLDER[subfolder]}`,
+});
+});
+//end create folder
+const data = {
+datasetId,
+dataName,
+userUpload,
+savePath,
+};
+const newData = new dataset(data);
+await newData.save();
+return responseSuccessWithData({ res, data: newData });
+} catch (error) {
+return responseServerError({ res, err: error.message });
+}
+};
 exports.readDataset = async (req, res) => {
-    try {
-        await dataset.findOne({ datasetId: req.body.datasetId })
-            .then(data => {
-                return responseSuccessWithData({ res, data: data })
-            })
-    } catch (error) {
-        return responseServerError({ res, err: error.message });
-    }
-
+try {
+await dataset.findOne({ datasetId: req.body.datasetId }).then((data) => {
+return responseSuccessWithData({ res, data: data });
+});
+} catch (error) {
+return responseServerError({ res, err: error.message });
 }
+};
 exports.deleteDataset = async (req, res) => {
-    try {
-        await dataset.findOneAndDelete({ datasetId: req.body.datasetId })
-            .then(() => {
-                return responseSuccess({ res });
-            })
-    } catch (err) {
-        return responseServerError({ res, err: err.message });
-    }
+try {
+//delete folder
+const root = path.resolve("./");
+const dataDir = removeDir({
+dir: root + `/${DATA_FOLDER}/${req.body.datasetId}`,
+});
+//end delete folder
+await dataset
+.findOneAndDelete({ datasetId: req.body.datasetId })
+.then(() => {
+return responseSuccess({ res });
+});
+} catch (err) {
+return responseServerError({ res, err: err.message });
 }
+};
+
--- src/lab/lab.controller.js
+++ src/lab/lab.controller.js
@@ -1,96 +1,107 @@
+
 const Joi = require("joi");
 require("dotenv").config();
 const { v4: uuid } = require("uuid");
 const Lab = require("./lab.model");
-const { responseServerError, responseInValid,responseSuccess, responseSuccessWithData } = require("../../helpers/ResponseRequest");
-const path = require('path')
+const {
+responseServerError,
+responseInValid,
+responseSuccess,
+responseSuccessWithData,
+} = require("../../helpers/ResponseRequest");
+const path = require("path");
 const { LAB_FOLDER, LAB_SUBFOLDER } = require("../../helpers/constant");
-const { getDir } = require('../../helpers/file');
+const { getDir,removeDir } = require("../../helpers/file");
 
 const labCreateSchema = Joi.object().keys({
-  labName: Joi.string().required(),
-  userCreated: Joi.string().required(),
+labName: Joi.string().required(),
+userCreated: Joi.string().required(),
 });
 
 const labUpdateSchema = Joi.object().keys({
-  labId: Joi.string().required(),
-  labName: Joi.string().required(),
-  userCreated: Joi.string().required()
-})
+labId: Joi.string().required(),
+labName: Joi.string().optional(),
+configPath: Joi.string().optional(),
+});
 
 exports.createLab = async (req, res) => {
-  try {
-    const result = labCreateSchema.validate(req.body);
-    if (result.error) {
-      return responseServerError({ res, err: result.error.message });
-    }
-    const { labName, userCreated } = req.body;
-    const labId = uuid();
-    // create folder
-    const root = path.resolve('./');
-    const dir = getDir({ dir: root + `/${LAB_FOLDER}` });
-    const labDir = getDir({ dir: root + `/${LAB_FOLDER}/${labId}` });
-    const subLabDir = {};
-    Object.keys(LAB_SUBFOLDER).map(subfolder => {
-      const dirPath = `/${LAB_FOLDER}/${labId}/${LAB_SUBFOLDER[subfolder]}`;
-      getDir({ dir: root + dirPath });
-      subLabDir[subfolder] = dirPath;
-    })
-    // done create
+try {
+const result = labCreateSchema.validate(req.body);
+if (result.error) {
+return responseServerError({ res, err: result.error.message });
+}
+const { labName, userCreated } = req.body;
+const labId = uuid();
+// create folder
+const root = path.resolve("./");
+// const dir = getDir({ dir: root + `/${LAB_FOLDER}` });
+const labDir = getDir({ dir: root + `/${LAB_FOLDER}/${labId}` });
+const subLabDir = {};
+Object.keys(LAB_SUBFOLDER).map((subfolder) => {
+const dirPath = `/${LAB_FOLDER}/${labId}/${LAB_SUBFOLDER[subfolder]}`;
+getDir({ dir: root + dirPath });
+subLabDir[subfolder] = dirPath;
+});
+// done create
 
-    // Thêm vào lab entity
-    const labData = {
-      labId,
-      labName,
-      userCreated,
-      trainLogPath: subLabDir["trainLogPath"],
-      testLogPath: subLabDir["testLogPath"],
-      trainedModelPath: subLabDir["trainedModelPath"]
-    };
-    const newLab = new Lab(labData);
-    await newLab.save();
+// Thêm vào lab entity
+const labData = {
+labId,
+labName,
+userCreated,
+trainLogPath: subLabDir["trainLogPath"],
+testLogPath: subLabDir["testLogPath"],
+trainedModelPath: subLabDir["trainedModelPath"],
+};
+const newLab = new Lab(labData);
+await newLab.save();
 
-    return responseSuccessWithData({ res, data: newLab });
-  } catch (error) {
-    return responseServerError({ res, err: error.message });
-  }
+return responseSuccessWithData({ res, data: newLab });
+} catch (error) {
+return responseServerError({ res, err: error.message });
+}
 };
 
 exports.updateLab = async (req, res) => {
-  try {
-    const result = labUpdateSchema.validate(req.body)
-    if (result.error) {
-      return responseServerError({ res, err: result.error.message })
-    }
-    let data = await Lab.findOneAndUpdate(req.body.datasetId, req.body, {
-      new: true
-    })
-    return responseSuccessWithData({
-      res, data: data
-    })
-  } catch (error) {
-    return responseServerError({ res, err: error.message });
-  }
+try {
+const result = labUpdateSchema.validate(req.body);
+if (result.error) {
+return responseServerError({ res, err: result.error.message });
 }
+delete result.value.labId;
+let data = await Lab.findOneAndUpdate(req.body.labId, result.value, {
+new: true,
+});
+return responseSuccessWithData({
+res,
+data: data,
+});
+} catch (error) {
+return responseServerError({ res, err: error.message });
+}
+};
 
 exports.readLab = async (req, res) => {
-  try {
-    await Lab.findOne({ labId: req.body.labId })
-    .then(data => {
-        return responseSuccessWithData({ res, data: data })
-    })
-  } catch (error) {
-    return responseServerError({ res, err: error.message });
-  }
+try {
+await Lab.findOne({ labId: req.body.labId }).then((data) => {
+return responseSuccessWithData({ res, data: data });
+});
+} catch (error) {
+return responseServerError({ res, err: error.message });
 }
+};
 
 exports.deleteLab = async (req, res) => {
-  try {
-      await Lab.findOneAndDelete({ labId: req.body.labId })
-          .then(() => {
-              return responseSuccess({ res });
-          })
-  } catch (err) {
-      return responseServerError({ res, err: err.message });
-  }
+try {
+// delete folder
+const root = path.resolve("./");
+const labDir = removeDir({ dir: root + `/${LAB_FOLDER}/${req.body.labId}` });
+// done delete
+await Lab.findOneAndDelete({ labId: req.body.labId }).then(() => {
+return responseSuccess({ res });
+});
+} catch (err) {
+return responseServerError({ res, err: err.message });
 }
+};
+
