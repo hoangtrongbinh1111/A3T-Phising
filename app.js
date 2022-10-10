eb287d9143772c83fdbfef4d8e965d9d1295af98
Nguyễn Hoàng Hiệp <hoanghiepdn97@gmail.com>
2022-10-10 08:53:53 +0700

    Update

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ const blacklistRoutes = require("./routes/blacklist");
 const modelRoutes = require("./routes/model");
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    dbName: process.env.DB_NAME,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connection Success.");
-  })
-  .catch((err) => {
-    console.error("Mongo Connection Error", err);
-  });
+    .connect(process.env.MONGO_URI, {
+        dbName: process.env.DB_NAME,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database connection Success.");
+    })
+    .catch((err) => {
+        console.error("Mongo Connection Error", err);
+    });
 
 const app = express();
 
@@ -37,24 +37,23 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-(async () => {
-  app.get("/ping", (req, res) => {
-    return res.status(200).send({
-      status: true,
-      message: "Server is healthy",
+(async() => {
+    app.get("/ping", (req, res) => {
+        return res.status(200).send({
+            status: true,
+            message: "Server is healthy",
+        });
     });
-  });
-  app.use("/api/v1/data", dataRoutes);
-  app.use("/api/v1/lab", labRoutes);
-  app.use("/api/v1/users", authRoutes);
-  app.use("/api/v1/admin", adminRoutes);
-  app.use("/api/v1/log", logRoutes);
-  app.use("/api/v1/blacklist", blacklistRoutes);
-  app.use("/api/v1/model", modelRoutes);
+    app.use("/api/v1/data", dataRoutes);
+    app.use("/api/v1/lab", labRoutes);
+    app.use("/api/v1/users", authRoutes);
+    app.use("/api/v1/admin", adminRoutes);
+    app.use("/api/v1/log", logRoutes);
+    app.use("/api/v1/blacklist", blacklistRoutes);
+    app.use("/api/v1/model", modelRoutes);
 
-  const PORT = process.env.PORT || 8686;
-  app.listen(PORT, () => {
-    console.log("Server started listening on PORT : " + PORT);
-  });
+    const PORT = process.env.PORT || 8686;
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log("Server started listening on PORT : " + PORT);
+    });
 })();
-
